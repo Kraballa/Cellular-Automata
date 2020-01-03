6d2637713414a408e8bebef3dfb4864aa521589c
Vinzent Brömauer <vinz.corno@web.de>
2020-01-03 15:34:55 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SandBox
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Engine(150,100,8))
+                game.Run();
+        }
+    }
+}
